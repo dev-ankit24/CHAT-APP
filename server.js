81e5f6776e874788e9870a65f3aef487bdd736a2
dev-ankit24 <ankitkumar839282@gmail.com>
2024-07-27 09:51:23 +0530

    fix bug

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const io= require("socket.io")(8000,{
-    cors:"*"
+    cors:"http://localhost:5500"
 }
 )
 const users={}
@@ -8,6 +8,7 @@ io.on("connect",(socket)=>{
     socket.on("user-joined", (name)=>{
         users[socket.id]=name
         socket.broadcast.emit("new-user-joined",name)
+        console.log(users);
     })
 
     socket.on("send",(message)=>{
--- views/JS/client.js
+++ views/JS/client.js
@@ -1,1 +1,28 @@
-const socket =io("http://localhost:8000")
+const socket = io("http://localhost:8000")
+ 
+const name= prompt("Enter the YOUR name to Join the chat :")
+socket.emit("user-joined",name)
+
+let first =document.getElementById(".first")
+function generateMessage(side,message){
+    let div= document.createElement("div")
+
+    div.classList.add("alert")
+    div.innerHTML=message
+    if(side==="left"){
+        div.classList.add("alert-primary")
+        div.classList.add("left")
+    }
+    else if(side==="right"){
+        div.classList.add("alert.secondry")
+        div.classList.add("right")
+    }
+    else{
+        div.classList.add("alert-info")
+        div.classList.add("center")
+    }
+    first.append(div)
+}
+socket.on("new-user-joined", name=>{
+    generateMessage("center", `${name} joined the chat`)
+})
