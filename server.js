1e1ebd87c11088a1c39c305fba9a25bf1f987400
dev-ankit24 <ankitkumar839282@gmail.com>
2024-08-17 10:11:19 +0530

    create clint script

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ io.on("connect",(socket)=>{
     })
 
     socket.on("send",(message)=>{
-        socket.broadcast("receive",{name:users[socket.id], message})
+        socket.broadcast.emit("receive",({name:users[socket.id], message}))
     })
 
     socket.on("disconnect",()=>{
--- views/JS/client.js
+++ views/JS/client.js
@@ -14,7 +14,7 @@ function generateMessage(side,message){
         div.classList.add("left")
     }
     else if(side==="right"){
-        div.classList.add("alert.secondry")
+        div.classList.add("alert.warning")
         div.classList.add("right")
     }
     else{
@@ -24,15 +24,33 @@ function generateMessage(side,message){
     first.appendChild(div)
 }
 socket.on("new-user-joined", name=>{
+    if(name){
     generateMessage("center", `${name} joined the chat`)
+
+    }
 })
 
 // send input function 
  function sendMessage(){
     let input =document.getElementById("message")
-    if(input.value?.lenght){
+    console.log(input.value);
+    
+    if(input.value?.length){
         generateMessage("right", `${input.value}: You`)
         socket.emit("send",input.value)
         input.value=""
     }
  }
+
+ socket.on("receive",({name,message})=>{
+    console.log(name);
+    
+    generateMessage("left",`${name} : ${message}`)
+ })
+
+ socket.on("user-left",(name)=>{
+    if(name){
+    generateMessage("center",`${name} Left the chat`)
+
+    }
+})
